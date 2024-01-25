cfa3344074aa2ac75df6a12e487e39502553cd50
Marlu <marlusilva@gmail.com>
2024-01-25 14:06:45 -0300

    MergeSort

--- listaLivros.js
+++ listaLivros.js
@@ -1,16 +1,48 @@
-const precosLivros=[
-    {titulo:"JavaScript",
-    preco: 25},
-    {titulo:"PHP",
-    preco: 15},
-    {titulo:"Elixir",
-    preco: 50},
-    {titulo:"Java",
-    preco: 30},
-    {titulo:"Go",
-    preco: 45},
-    {titulo:"Python",
-    preco: 20}
-] ;
-
-module.exports = precosLivros;
+const listaLivros = [
+    {
+      titulo: "Go",
+      preco: 45
+    },
+    {
+      titulo: "C++",
+      preco: 35
+    },
+    {
+      titulo: "Java",
+      preco: 30
+    },
+    {
+    titulo: "PHP",
+    preco: 15
+    },
+    {
+      titulo: "Elixir",
+      preco: 50
+    },
+    {
+      titulo: "Rust",
+      preco: 22
+    },
+    {
+      titulo: "Scala",
+      preco: 40
+    },
+    {
+      titulo: "Ruby",
+      preco: 28
+    },
+    {
+      titulo: "JavaScript",
+      preco: 25
+    },
+    {
+      titulo: "C#",
+      preco: 33
+    },
+    {
+      titulo: "Python",
+      preco: 20
+    },
+  ]
+precosLivros=listaLivros;
+module.exports = listaLivros,precosLivros;
